b03019209e30f771459cb22166501d0e7b1b0a13
Angel Perez <iAngel.p93@gmail.com>
2024-11-12 16:41:52 +0100

    Use Deface DSL to comply with Zeitwerk

--- app/overrides/decorate_admin_products_form.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/products/_form",
-  name: "multi_domain_admin_product_form_meta",
-  insert_bottom: "[data-hook='admin_product_form_meta']",
-  partial: "spree/admin/products/stores",
-  disabled: false
-)
